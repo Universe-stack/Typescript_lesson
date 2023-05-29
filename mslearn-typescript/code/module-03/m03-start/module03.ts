61101e03463ad6ad6adb435e6ef8058b0aceaad1
Kachi <justicechinedu156@gmail.com>
2023-05-29 14:02:01 +0100

    interface assignments

--- mslearn-typescript/code/module-03/m03-start/module03.ts
+++ mslearn-typescript/code/module-03/m03-start/module03.ts
@@ -4,33 +4,42 @@
 /*  EXERCISE 1
     TODO: Declare the Loan interface. */
 
+    interface Loan {
+        principal:number,
+        interestRate: number
+    }
+
 
 /*  TODO: Declare the ConventionalLoan interface. */
+
+    interface ConventionalLoan extends Loan {
+        months:number
+    }
 
 
 
 /*  TODO: Update the calculateInterestOnlyLoanPayment function. */
 
-function calculateInterestOnlyLoanPayment(principle, interestRate) {
+function calculateInterestOnlyLoanPayment(loanTerms:Loan):string {
     // Calculates the monthly payment of an interest only loan
-    let interest = interestRate / 1200; // Calculates the Monthly Interest Rate of the loan
+    let interest = loanTerms.interestRate / 1200; // Calculates the Monthly Interest Rate of the loan
     let payment;
-    payment = principle * interest;
+    payment = loanTerms.principal * interest;
     return 'The interest only loan payment is ' + payment.toFixed(2);
 }
 
 /*  TODO: Update the calculateConventionalLoanPayment function. */
 
-function calculateConventionalLoanPayment(principle, interestRate, months) {
+function calculateConventionalLoanPayment(loanTerms:ConventionalLoan) {
     // Calculates the monthly payment of a conventional loan
-    let interest = interestRate / 1200; // Calculates the Monthly Interest Rate of the loan
+    let interest = loanTerms.interestRate / 1200; // Calculates the Monthly Interest Rate of the loan
     let payment;
-    payment = principle * interest / (1 - (Math.pow(1 / (1 + interest), months)));
+    payment = loanTerms.principal * interest / (1 - (Math.pow(1 / (1 + interest), loanTerms.months)));
     return 'The conventional loan payment is ' + payment.toFixed(2);
 }
 
-let interestOnlyPayment = calculateInterestOnlyLoanPayment(30000, 5);
-let conventionalPayment = calculateConventionalLoanPayment(30000, 5, 180);
+let interestOnlyPayment = calculateInterestOnlyLoanPayment({principal:3000,interestRate:5});
+let conventionalPayment = calculateConventionalLoanPayment({principal:30000,interestRate: 5, months:180});
 
 console.log(interestOnlyPayment);     //* Returns "The interest only loan payment is 125.00" 
 console.log(conventionalPayment);     //* Returns "The conventional loan payment is 237.24" 
--- myInterface.ts
+++ myInterface.ts
@@ -24,3 +24,45 @@ interface Administrator extends Customer {
 //const hitesh: Customer={dbId: 22,email:"h@h",userId:2342, startTrial(){return ""}, beginTrial(){ return 3}, getCoupon:(name:"Justicew", value:12)=>{return 10}, githubToken: "github"}
 const hitesh: Administrator={dbId: 22,email:"h@h",userId:2342, startTrial(){return ""}, beginTrial(){ return 3}, getCoupon:(name:"Justicew", value:12)=>{return 10}, githubToken: "github", role:"admin"}
 
+interface IceCream {
+    flavor?:string;
+    scoops:number;
+    instructions?:string
+}
+
+interface Sundae extends IceCream{
+    sauce:'chocolate'|'caramel'| 'cranberry';
+    nuts?:boolean;
+    instructions?:string;
+}
+
+let myIceCream:Sundae = {
+    flavor:"vanilla",
+    scoops:12,
+    sauce:'cranberry',
+    nuts:true
+}
+
+console.log(myIceCream.flavor);
+
+let tooManyScops=(dessert:Sundae)=>{
+    if(dessert.scoops>56) {
+        return dessert.scoops + 'is too many scoops'
+    }else {
+        return 'Your order would be ready soon';
+    }
+}
+
+console.log(tooManyScops({flavor:'Vanilla',scoops:23,sauce:'chocolate'}))
+
+
+//indexable types
+
+interface IceCreamArray {
+    [index:number]:string
+}
+
+let myIceCreamArray:IceCreamArray;
+myIceCreamArray= ['choco','vanilla','cranberry'];
+let myStr:string=myIceCreamArray[0]
+console.log(myStr)
